04aa26ee060547a91cee9b8aba3af816ddaefb51
Mickaël Canouil <8896044+mcanouil@users.noreply.github.com>
2026-09-06 23:51:22 +0200

    feat: declare the vendored modules in a manifest (#43)

--- _extensions/letter/letter.lua
+++ _extensions/letter/letter.lua
@@ -9,7 +9,7 @@
 --- LaTeX template can render them without bespoke escaping in the partials.
 
 local EXTENSION_NAME = 'letter'
-local log = require(quarto.utils.resolve_path('_modules/logging.lua'):gsub('%.lua$', ''))
+local log = require(quarto.utils.resolve_path('_vendor/quarto-lua-modules/logging.lua'):gsub('%.lua$', ''))
 
 --- Convert a metadata value to a trimmed string.
 --- @param value any Pandoc metadata value or nil
